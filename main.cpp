af96ca0a9a6a0a248eaf6f5787e4b39ccc5aea76
Eric Moessner <emoessne@gmail.com>
2024-06-16 12:04:47 -0700

    Add getline() and std::ws

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ int main()
 {
 	std::string myString{ "Hello World!" };
 
+	myString = "Eric";
+
+	std::cout << myString << '\n';
+	std::getline(std::cin >> std::ws, myString);
 	std::cout << myString << '\n';
 
 	return 0;
